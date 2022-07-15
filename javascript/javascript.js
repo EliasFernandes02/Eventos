d5094922213e36f8ba2f987e9c2c9cca18479cb7
Elias Fernandes <eliasneto0014@gmail.com>
2022-07-15 10:49:25 -0300

    segundo commit

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,3 +6,34 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+
+menu.onclick = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween:20,
+    grabCursos:true,
+    loop:true,
+    pagination: {
+
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+     breakpoints: {
+        640: {
+          slidesPerView: 1,
+          
+        },
+        768: {
+          slidesPerView: 2,
+          
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
