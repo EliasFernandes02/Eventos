bfc830e0366adf186e011813d9f053464f19fb8a
Elias Fernandes <eliasneto0014@gmail.com>
2022-07-18 14:30:58 -0300

    final commit

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -37,3 +37,29 @@ var swiper = new Swiper(".home-slider", {
         },
       },
   });
+
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween:20,
+    grabCursos:true,
+    loop:true,
+    pagination: {
+
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+     breakpoints: {
+        640: {
+          slidesPerView: 1,
+          
+        },
+        768: {
+          slidesPerView: 2,
+          
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
